886a0a2e7b489e45f7e2bd8c79d4e32b81499ab4
rhaney93 <ryanhaney0493@gmail.com>
2022-06-08 11:54:26 -0600

    started adding unit tests for "Shopping Cart Item"

---
+++ Deliverable2_USETHIS/CKK.Logic/ShoppingCartTest/UnitTest1.cs
@@ -0,0 +1,34 @@
+using System;
+using CKK.Logic.Models;
+using Xunit;
+
+namespace ShoppingCartTest
+{
+    public class ShoppingCartItem
+    {
+        private Customer _customer;
+        private ShoppingCartItem _product1;
+        private ShoppingCartItem _product2;
+        private ShoppingCartItem _product3;
+
+        /*[Fact]
+        public void Constructor_CustomerIdReturnSameAsInput()
+        {
+            //Assemble
+            Customer testCustomer = new Customer();
+            testCustomer.GetId();
+
+            //Act
+
+            //Assert
+
+        }*/
+
+        [Fact]
+        public int GetCustomerId
+        {
+
+        }
+            
+    }
+}
